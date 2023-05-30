b605a5e8185d284e5a21db164267282e8f67070e
Anatoli Pavlov <18app81@gmail.com>
2023-05-30 19:48:09 +0300

    Add views and controllers
    404 view
    catalog view
    details view

--- index.js
+++ index.js
@@ -3,12 +3,25 @@ const hbs = require('express-handlebars').create({
     extname: '.hbs',
 });
 
+const homeController = require('./controllers/homeController');
+const defaultController = require('./controllers/defaultController');
+const catalogController = require('./controllers/catalogController');
+
 const app = express();
 
 app.engine('hbs', hbs.engine);
 app.set('view engine', '.hbs');
 
-app.use(express.urlencoded({extended: true}));
-app.use('/static', express.static('static')); 
+app.use(express.urlencoded({ extended: true }));
+app.use('/static', express.static('static'));
+
+app.use(homeController);
+app.use('/catalog', catalogController);
+
+
+
+
+// last controller
+app.all('*', defaultController);
 
 app.listen(3000, () => console.log('Server is listening on port 3000...'));
