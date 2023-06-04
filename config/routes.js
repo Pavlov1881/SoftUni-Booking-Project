8aaade6837b4363fc2752558a4c258ddda4c2488
Anatoli Pavlov <18app81@gmail.com>
2023-06-04 10:55:57 +0300

    Add Mongoose
    Add Config folder for express

---
+++ config/routes.js
@@ -0,0 +1,16 @@
+const homeController = require('../controllers/homeController');
+const catalogController = require('../controllers/catalogController');
+const createController = require('../controllers/createController');
+const defaultController = require('../controllers/defaultController');
+
+
+
+module.exports = (app) => {
+
+    app.use(homeController);
+    app.use('/catalog', catalogController);
+    app.use('/create', createController);
+    // TODO attach other controllers
+
+    app.all('*', defaultController);
+}
